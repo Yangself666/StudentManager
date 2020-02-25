771c8b556c8e07a00dafecb205233784a7a4e87d
yangself <2579987682@qq.com>
2020-02-25 18:15:19 +0800

    初步开始

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
-//app.js
 App({
+  globalData:{
+    openid:null,
+    appid:"wxb4af88d61d8255aa",
+    secret:"a4c3744b257b5b68403c37ab142cda42"
+  },
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+      success: (res) => {
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data:{
+            appid:app.globalData.appid,
+            secret:app.globalData.secret,
+            js_code:res.code,
+            grant_type:"authorization_code"
+          },
+          method:"GET",
+          success(res){
+            app.globalData.openid = res.data.openid
+            console.log("成功")
+            console.log("aaa")
+            if (this.getUserOpenid ){
+              this.getUserOpenid(res)
             }
-          })
-        }
-      }
+            console.log("aaa")
+          }
+        })
+      },
     })
   },
-  globalData: {
-    userInfo: null
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
   }
+  
 })
